c0ed567589a4af4ebc0c4399c1279db0c6939ef3
Dottore89 <115156475+Dottore89@users.noreply.github.com>
2024-11-13 11:59:25 +0100

    Van Detail Created

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,51 +1,33 @@
 import React from "react"
-
-/**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- * 
- * Hints:
- * 1. Use `fetch(/api/vans)` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
- * 3. You may get an error saying "console.groupCollapsed is not
- *    a function". You can ignore it for now.
- */
+import { Link } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
-
-    React.useEffect(function() {
+    React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
-    
-    const vanElements = vans.map(function(van) {
-        return <div key={van.id} className="van-item">
-                    <img src={van.imageUrl}/>
-                    <h4>{van.name}</h4>
-                    <p>${van.price}/day</p>
-                    <button className={van.type}>{van.type}</button>
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
                 </div>
-    })
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
 
     return (
-        <>
-        <div className="vans-header">
-            <h2>Explore our van options</h2>
-            <div className="filters-div">
-                <button>Simple</button>
-                <button>Luxury</button>
-                <button>Rugged</button>
-                <h3>Clear filters</h3>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
             </div>
         </div>
-        <div className="van-list">
-            {vanElements}
-        </div>
-        </>
     )
 }
